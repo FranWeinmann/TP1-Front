c16b2ac4ee1a6d8f82a76851afb3d7f9c2bde0e3
FranWeinmann <weinmann.fran@gmail.com>
2026-03-31 15:52:19 -0300

    adding copy function

--- password-generator-pro/utils/func.js
+++ password-generator-pro/utils/func.js
@@ -1,4 +1,18 @@
-export function generatePassword(length, uppercase, lowercase, numbers, symbols){
+export function generatePassword(length, uppercase, lowercase, numbers, symbols) {
     let chars = "";
-    if (uppercase) chars += "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+
+    if (uppercase) chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (lowercase) chars += "abcdefghijklmnopqrstuvwxyz";
+    if (numbers) chars += "0123456789";
+    if (symbols) chars += "!@#$%^&*()_+-=[]{}|;:,.<>?";
+    if (chars.length === 0) {
+        throw new Error("Selecciona al menos ");
+    }
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        password += chars[randomIndex];
+    }
+
+    return password;
 }
